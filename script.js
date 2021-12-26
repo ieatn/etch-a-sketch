7de99b8c99a831aa358f3cac8b81c61d7283c1b4
Dennis Zhang <81874063+ieatn@users.noreply.github.com>
2021-12-26 11:09:22 -0800

    Update script.js

--- script.js
+++ script.js
@@ -1,46 +1,64 @@
-const DEFAULT_SQUARES = 4
-let canvas = document.querySelector("#grid");
+const DEFAULT = 4
+let canvas = document.querySelector('#container')
+let numberOfCells = DEFAULT;
+let side = 700/numberOfCells
+var slider = document.querySelector('#range')
+var output = document.querySelector('#range-value')
+output.innerHTML = slider.value
 
-let numberOfCells = DEFAULT_SQUARES;
-let side = 700 / numberOfCells;
-var slider = document.getElementById("cellNumber");
-var output = document.getElementById("sliderValue");
-output.innerHTML = slider.value; // Display the default slider value
-
-// Update the current slider value (each time you drag the slider handle)
 slider.oninput = function() {
     output.innerHTML = this.value;
     numberOfCells = this.value;
     side = 700 / numberOfCells;
 }
 
-createCells(numberOfCells, side, canvas);
-
-const reset = document.querySelector("#reset");
-reset.addEventListener("click", resetCells);
-
 let brush = 1;
-const blackBrush = document.querySelector("#blackBrush");
-blackBrush.addEventListener("click", () => {
-    brush = 0;
+const blackBrush = document.querySelector('#black')
+blackBrush.addEventListener('click', () => {
+    brush = 0
     blackBrush.style.background = "hsl(149, 100%, 51%)";
     rainbowBrush.style.background = "#B5FFD9";
     eraser.style.background ="#B5FFD9";
-});
-const rainbowBrush = document.querySelector("#rainbowBrush");
-rainbowBrush.addEventListener("click", () => {
+})
+const rainbowBrush = document.querySelector('#color')
+rainbowBrush.addEventListener('click', () => {
     brush = 1;
     blackBrush.style.background = "#B5FFD9";
     rainbowBrush.style.background = "hsl(149, 100%, 51%)";
     eraser.style.background ="#B5FFD9";
-});
-const eraser = document.querySelector("#eraser");
-eraser.addEventListener("click", () => {
+})
+const eraser = document.querySelector('#eraser')
+eraser.addEventListener('click', () => {
     brush = 2;
     blackBrush.style.background = "#B5FFD9";
     rainbowBrush.style.background = "#B5FFD9";
     eraser.style.background ="hsl(149, 100%, 51%)";
-});
+})
+const reset = document.querySelector('#reset')
+reset.addEventListener('click', resetCells)
+
+
+createCells(numberOfCells, side, canvas)
+
+function createCells(numberOfCells, side, canvas) {
+    for (let i = 1; i <= numberOfCells; i++) {
+        for (let j = 1; j <= numberOfCells; j++) {
+            const cell = document.createElement('div')
+            cell.classList.add('box')
+            canvas.appendChild(cell)
+        }
+    }
+    let cells = document.querySelectorAll('.box')
+    cells.forEach((cell) => {
+        cell.style.width = side + 'px';
+        cell.style.height = side + 'px';
+    })
+}
+
+
+
+
+
 
 paintCanvas();
 
@@ -61,6 +79,7 @@ function paint(e) {
         this.style.background = "white";
     }
 }
+
 function getRandomColor() {
     let letters = '0123456789ABCDEF';
     let color = '#';
@@ -69,27 +88,12 @@ function getRandomColor() {
     }
     return color;
 }
-function createCells(numberOfCells, side, canvas) {
-    for (let i = 1; i <= numberOfCells; i++) {
-        for (let k = 1; k <= numberOfCells; k++) {
-            const cell = document.createElement("div");
-            cell.classList.add("box");
-            canvas.appendChild(cell);
-        }
-    }
 
-    let cells = document.querySelectorAll(".box");
-
-    cells.forEach((cell) => {
-        cell.style.width = side + "px";
-        cell.style.height = side + "px";
-    });
-}
 function resetCells() {
-    const elements = document.getElementsByClassName("box");
+    const elements = document.getElementsByClassName('box')
     while (elements.length > 0) {
-        elements[0].parentNode.removeChild(elements[0]);
+        elements[0].parentNode.removeChild(elements[0])
     }
-    createCells(numberOfCells, side, canvas);
-    paintCanvas();
+    createCells(numberOfCells, side, canvas)
+    paintCanvas()
 }
